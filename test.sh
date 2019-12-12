9d08880be2740b722cac108fa4265a8b1f78e6d1
ojdo <jdorfner@gmail.com>
2019-12-12 17:45:21 +0100

    test.sh: Yield error in case of error

--- test.sh
+++ test.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # small check that all models still compile
+error_code=0
 for model_file in *.mod;
 do
     if !(_=$(glpsol -m $model_file 2>&1)); then
         printf "Error in '$model_file'\n"
+	error_code=1
     fi
 done
+exit $error_code
